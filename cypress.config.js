3b0cb1280aaa2a1bc17735d8dc616dcb633a67ab
Maria <moreva7530@gmail.com>
2024-08-16 18:37:12 +0500

    2

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     projectId: 'your_project_id_here',
+    pageLoadTimeout:1000000,
   },
-};
+});
