1dcbceffa4dc087ff479ef8895d48f32f83bd843
Maria <moreva7530@gmail.com>
2024-08-23 14:52:44 +0500

    adding cross-browser

--- cypress/e2e/2-task/contactInfoCheck.cy.js
+++ cypress/e2e/2-task/contactInfoCheck.cy.js
@@ -1,7 +1,47 @@
 import React from 'react';
 import { mount } from '@cypress/react'; // или другой подходящий метод для монтирования компонентов
 ///<reference types="cypress" />
-describe('contactInfo', () => {
+describe('contactInfo', { browser: 'firefox' }, () => {
+    beforeEach(() => {
+        cy.visit('https://www.google.ru');
+        cy.get('.gLFyf').type('Byndyusoft')
+        .type('{enter}');
+        cy.get('#search').should('be.visible');
+        cy.get('#search a').first().invoke('removeAttr', 'target').click();
+    });
+
+    it('should render contact info', () => { 
+        cy.origin('https://byndyusoft.com', () => {
+        cy.get('.knowMore__container') 
+        .children('span').click() 
+        cy.get('.popup-callback__content').should('be.visible'); // просмотр элемента поп-апа  
+        cy.get('.popup-callback__contacts-tg') 
+        .invoke('attr', 'href') // Получите значение атрибута href 
+        .should('equal', 'http://t.me/alexanderbyndyu');
+        })
+    });
+});
+describe('contactInfo', { browser: 'chrome' }, () => {
+    beforeEach(() => {
+        cy.visit('https://www.google.ru');
+        cy.get('.gLFyf').type('Byndyusoft')
+        .type('{enter}');
+        cy.get('#search').should('be.visible');
+        cy.get('#search a').first().invoke('removeAttr', 'target').click();
+    });
+
+    it('should render contact info', () => { 
+        cy.origin('https://byndyusoft.com', () => {
+        cy.get('.knowMore__container') 
+        .children('span').click() 
+        cy.get('.popup-callback__content').should('be.visible'); // просмотр элемента поп-апа  
+        cy.get('.popup-callback__contacts-tg') 
+        .invoke('attr', 'href') // Получите значение атрибута href 
+        .should('equal', 'http://t.me/alexanderbyndyu');
+        })
+    });
+});
+describe('contactInfo', { browser: 'safari' }, () => {
     beforeEach(() => {
         cy.visit('https://www.google.ru');
         cy.get('.gLFyf').type('Byndyusoft')
